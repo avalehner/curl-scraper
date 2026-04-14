f5377ffd9dd2934ed7d8c28433c36a91c8184c58
Ava Lehner <avalehner@gmail.com>
2026-04-14 12:23:57 -0400

    initial commit

---
+++ target/debug/curl-scraper.d
@@ -0,0 +1,1 @@
+/Users/avalehner/Documents/Dev/curl-scraper/target/debug/curl-scraper: /Users/avalehner/Documents/Dev/curl-scraper/src/main.rs
---
+++ target/debug/deps/curl_scraper-2409d46e6970dd14.d
@@ -0,0 +1,5 @@
+/Users/avalehner/Documents/Dev/curl-scraper/target/debug/deps/curl_scraper-2409d46e6970dd14.d: src/main.rs
+
+/Users/avalehner/Documents/Dev/curl-scraper/target/debug/deps/curl_scraper-2409d46e6970dd14: src/main.rs
+
+src/main.rs:
