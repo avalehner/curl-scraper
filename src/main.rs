f7f7cdcad2c7a63f933da1b5a781b082ae96d78c
Ava Lehner <avalehner@gmail.com>
2026-04-14 13:38:28 -0400

    maid main function async

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(),Box<dyn std::error::Error>> { //() means if it working nothing meaningful will be returned. Box<dyn std::error::Error> meains it failed and here is some type of error
+    println!("Hello, world!"); 
+    Ok(()) //signals success 
 }
+
+//think of result as a container type with two slots, tells rust the two possible outcomes the functiona can return 
+//Result < SuccessType, ErrorType >
+
+
